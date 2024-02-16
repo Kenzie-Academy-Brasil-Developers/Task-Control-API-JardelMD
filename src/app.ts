0f3d5a14bc347c261a857b58304d086471364062
JardelMD <130687030+JardelMD@users.noreply.github.com>
2024-02-16 12:43:25 -0300

    feat: renable cors, initialize typescript and add bycriptjs lib

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import "express-async-errors";
 import "dotenv/config";
 import express, { json } from "express";
 import helmet from "helmet";
+import cors from "cors";
 import { HandleErrors } from "./middlewares/handleErrors.middleware";
 import { categoryRouter } from "./routes/categories.routes";
 import { taskRouter } from "./routes/tasks.routes";
 import { userRouter } from "./routes/user.routes";
 
 export const app = express();
+
+app.use(cors());
 
 app.use(helmet());
 
